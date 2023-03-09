80a7bf5182a2394475a21c51ebf5adedba7734f7
MMGHTeam <mmgh102@gmail.com>
2023-03-09 13:22:45 +0330

    First version

---
+++ src/main/java/ir/mmgh/springmvc/helloWorld/controller/HelloWorldController.java
@@ -0,0 +1,32 @@
+package ir.mmgh.springmvc.helloWorld.controller;
+
+import java.time.LocalDateTime;
+
+import ir.mmgh.springmvc.helloWorld.model.HelloWorld;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/helloworld")
+    public String handler(Model model) {
+
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World Example Using Spring MVC 5!!!");
+        helloWorld.setDateTime(LocalDateTime.now().toString());
+        model.addAttribute("helloWorld", helloWorld);
+        return "helloworld";
+    }
+
+    @RequestMapping("/helloworld2")
+    public String handler2(Model model) {
+
+        HelloWorld helloWorld2 = new HelloWorld();
+        helloWorld2.setMessage("Hello World Example Using Spring MVC 52!!!");
+        helloWorld2.setDateTime(LocalDateTime.now().toString());
+        model.addAttribute("helloWorld2", helloWorld2);
+        return "helloworld2";
+    }
+}
---
+++ src/main/java/ir/mmgh/springmvc/helloWorld/model/HelloWorld.java
@@ -0,0 +1,20 @@
+package ir.mmgh.springmvc.helloWorld.model;
+
+public class HelloWorld {
+
+    private String message;
+    private String dateTime;
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String getDateTime() {
+        return dateTime;
+    }
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+}
